7e2e11272d60de7150532dc4ed60e20db3b2a900
Leandro <32607@ma-web.nl>
2023-04-19 12:34:38 +0200

    les 4

--- colorpicker/colorpicker_1/js/main.js
+++ colorpicker/colorpicker_1/js/main.js
@@ -1,3 +1,4 @@
+ //geef class id's aan om te gebruiken in code hier onder
 class ColorCard {
     id;
     color;
@@ -6,20 +7,23 @@ class ColorCard {
     circle;
     text;
 
-    constructor(newId, newColor, addToList) {
+    constructor(newId, newColor, addToList) { //id's die je in de constructor kan gebruiken om iets aan te roepen
         //setting properties
         this.id = newId;
         this.color = newColor;
         this.addToList = addToList;
 
         //make htmlElement to render
+        //genereert de li
         this.htmlElement = document.createElement("li");
         this.htmlElement.classList = "colors__color";
 
+        //genereert de figure
         this.circle = document.createElement("figure");
         this.circle.classList = "colors__circle";
         this.circle.style.background = this.color;
 
+        //genereert de tekst
         this.text = document.createElement("p");
         this.text.innerText = "Copied";
         this.text.classList = "colors__text";
@@ -29,10 +33,14 @@ class ColorCard {
         this.render();
     }
 
+    //het onclick effect (coppied tekst verschijnt en de animatie speelt af)
     onHTMLElementClicked = () => {
         this.circle.classList.add("colors__circle--selected")
+        document.title = this.color;
+        window.navigator.clipboard.writeText(this.color)
     }
 
+    //alles bij elkaar word gerendered 
     render() {
         this.htmlElement.appendChild(this.circle);
         this.htmlElement.appendChild(this.text);
@@ -40,5 +48,35 @@ class ColorCard {
     }
 }
 
-const test = new ColorCard(101, "hsl(284,52%,36%)", document.getElementById("js--colors"));
+ //geef class id's aan om te gebruiken in code hier onder
+class ColorList {
+    id;
+    htmlElement;
+
+
+    constructor(newID){ //id's die je in de constructor kan gebruiken om iets aan te roepen
+        this.id = newId;
+        this.htmlElement = document.createElement("ul")
+        this.htmlElement.id = this.id;
+        document.querySelector("body").appendChild(this.htmlElement);
+    }
+}
+
+for(let i = 1; i < 101; i++){ //herhaalt 100x wat hier onder staat
+
+
+    let randomHue = Math.floor(Math.random() * (360 - 1) + 1); //maakt een random hue aan voor de kleur
+    let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%"; //maakt de saturation aan voor de kleur hierboven
+    let randomLightness = Math.floor(Math.random () * (100- 11) + 11) + "%"; //maakt de lightness aan om te bepalen hoe donker de kleur is 
+
+    //voegt de 3 dingen hierboven aan elkaar toe 
+    let hsl = `hsl(${randomHue}, ${randomSaturation}, ${randomLightness})`
+    
+    //verbind de js bestanden in de for loop aan "js--colors" in de scss/html
+    new ColorCard(i, hsl, document.getElementById("js--colors"));
+
+}
+
+
+
 
